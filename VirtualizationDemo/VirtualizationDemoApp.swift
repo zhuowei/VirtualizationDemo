272617dc896fb1010a1935bb44d28004593a48e9
Zhuowei Zhang <zhuoweizhang@yahoo.com>
2021-06-07 17:03:01 -0400

    Initial Commit

---
+++ VirtualizationDemo/VirtualizationDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  VirtualizationDemoApp.swift
+//  VirtualizationDemo
+//
+//  Created by Zhuowei Zhang on 2021-06-07.
+//
+
+import SwiftUI
+
+@main
+struct VirtualizationDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
